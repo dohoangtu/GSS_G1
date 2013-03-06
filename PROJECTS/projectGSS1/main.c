5cea19d9ac6e0748e519ddcef5211f55250cd4a6
Tong Vu Than Dan <tongvtdan@gmail.com>
2013-03-06 15:06:06 +0700

    Thay doi thong tin du an tai dau file main.c

--- PROJECTS/projectGSS1/main.c
+++ PROJECTS/projectGSS1/main.c
@@ -7,15 +7,8 @@
   * @brief   Main program body
   ******************************************************************************
   * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  * Ma chuong trinh dieu khien robot G1 cho he thong Robot phun thuoc BVTV
+  * <h2><center>&copy; COPYRIGHT 2012 Gremsy Co., Ltd</center></h2>
   ******************************************************************************  
   */ 
 /* Private define ------------------------------------------------------------------*/
